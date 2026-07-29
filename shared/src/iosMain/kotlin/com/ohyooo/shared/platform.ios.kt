3976c5291707edea6e740261ec3d1263d2d1efb8
i <i@ohyooo.com>
2026-07-29 22:56:06 +0800

    Add missing iOS font implementation

--- shared/src/iosMain/kotlin/com/ohyooo/shared/platform.ios.kt
+++ shared/src/iosMain/kotlin/com/ohyooo/shared/platform.ios.kt
@@ -1,7 +1,11 @@
 package com.ohyooo.shared
 
+import org.jetbrains.compose.resources.FontResource
+
 actual fun getPlatformName() = "iOS Main"
 
 actual fun openGitHub() {
     println(getPlatformName())
 }
+
+actual fun getFont(): FontResource? = null
